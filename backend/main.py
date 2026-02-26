2a6c28f5cba38a5dd05b6314ea626d0e51099020
ABISHEK R <abishek@doerstech.in>
2026-02-26 18:10:12 +0530

    Initial DevOps assignment with Kubernetes and CI/CD

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import os
+import psycopg2
+
+app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        dbname=DB_NAME
+    )
+
+@app.get("/")
+def read_root():
+    return {"message": "Backend is running"}
